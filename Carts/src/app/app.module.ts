bec0c55f4bc4092beb00893d3ed0cee30e2efcfc
Hank Liu <liu.7220@buckeyemail.osu.edu>
2021-09-25 09:49:43 -0400

    Hank
    add common components , summary, and payment components. Add bootstrap css

--- Carts/src/app/app.module.ts
+++ Carts/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentPageComponent } from './components/payment-page/payment-page.component';
+import { SummaryPageComponent } from './components/summary-page/summary-page.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentPageComponent,
+    SummaryPageComponent,
+    HeaderComponent,
+    FooterComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
