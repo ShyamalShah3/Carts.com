86b7fc20dc4c09c2894ea07dd436824d8d6ae57c
Hank Liu <liu.7220@buckeyemail.osu.edu>
2021-09-26 13:13:19 -0400

    Add routes in app-routing.module.ts

--- Carts/src/app/app-routing.module.ts
+++ Carts/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentPageComponent } from './components/payment/payment-page.component';
+import { SummaryPageComponent } from './components/summary/summary-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'payment', component: PaymentPageComponent},
+{path: 'summary', component:SummaryPageComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], 
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- Carts/src/app/app.module.ts
+++ Carts/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PaymentPageComponent } from './components/payment-page/payment-page.component';
-import { SummaryPageComponent } from './components/summary-page/summary-page.component';
+import { PaymentPageComponent } from './components/payment/payment-page.component';
+import { SummaryPageComponent } from './components/summary/summary-page.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
