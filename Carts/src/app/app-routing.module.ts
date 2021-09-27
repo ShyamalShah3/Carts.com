93e2001c0629e51c74f8c35339a742d9c725e088
Hank Liu <liu.7220@buckeyemail.osu.edu>
2021-09-26 20:12:34 -0400

    add router outlet

--- Carts/src/app/app-routing.module.ts
+++ Carts/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './components/catalog/catalog.component';
 import { PaymentPageComponent } from './components/payment/payment-page.component';
 import { SummaryPageComponent } from './components/summary/summary-page.component';
 
+
 const routes: Routes = [
+{path: '',redirectTo:'/catalog',pathMatch:'full'},
 {path: 'payment', component: PaymentPageComponent},
-{path: 'summary', component:SummaryPageComponent}
+{path: 'summary', component:SummaryPageComponent},
+{path:'catalog', component:CatalogComponent}
 ];
 
 @NgModule({
