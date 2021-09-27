40d909f17fb20aa370e8076fec48873238652aa4
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2021-09-26 20:15:51 -0400

    Updated app.module.ts

--- Carts/src/app/app.module.ts
+++ Carts/src/app/app.module.ts
@@ -7,7 +7,13 @@ import { PaymentPageComponent } from './components/payment/payment-page.componen
 import { SummaryPageComponent } from './components/summary/summary-page.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { FooterComponent } from './components/common/footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { FiltersComponent } from './components/catalog/filters/filters.component';
+import { ItemsComponent } from './components/catalog/items/items.component';
+import { CartComponent } from './components/catalog/cart/cart.component';
+import { CartItemComponent } from './components/catalog/cart/cart-item/cart-item.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
 
 
 
@@ -24,7 +30,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ItemsComponent,
     CartComponent,
     CartItemComponent,
-    ItemComponent,
+    ItemsComponent,
     ShippingComponent,
 
 
