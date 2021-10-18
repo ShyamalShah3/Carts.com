1b1abe139ca7234e3068ca21592c4edb571021b0
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2021-10-18 11:44:16 -0400

    Inventory Management Service Functional
    Also removed inventory management service java code from this repo

--- Carts/inventory-management/src/main/java/edu/osu/cse5234/business/inventorymanagement/BaseApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.osu.cse5234.business.inventorymanagement;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class BaseApplication extends Application {
-
-}
--- Carts/src/app/services/product.service.ts
+++ Carts/src/app/services/product.service.ts
@@ -11,14 +11,7 @@ export class ProductService {
 
   @Injectable()
 
-  products: Product[] = [
-    new Product(1, "Two-Tier Cart", "This is a two tier shopping cart.", 100, "https://m.media-amazon.com/images/I/61COlmcV7yL._SL1000_.jpg"),
-    new Product(2, "Heavy Duty Small Cart", "This is a small heavy duty metal shopping cart.", 190.5, "https://cdna3.zoeysite.com/Adzpo594RQGDpLcjBynL1z/cache=expiry:31536000//auto_image/compress/https://s3.amazonaws.com/zcom-media/sites/a0i0L00000TM7fPQAT/media/catalog/product/s/m/small-metal-shopping-cart-angle.jpg"),
-    new Product(3, "Liquor Store Cart", "This is a modern Liquor Store Cart.", 130, "https://cdna3.zoeysite.com/Adzpo594RQGDpLcjBynL1z/cache=expiry:31536000//auto_image/compress/https://s3.amazonaws.com/zcom-media/sites/a0i0L00000TM7fPQAT/media/catalog/product/m/o/modern-liquor-store-shopping-cart-30.gif.gif"),
-    new Product(4, "Kids Miniature Shopping Cart", "This is a miniature shopping cart intended to be used by children.", 70.0, "https://m.media-amazon.com/images/I/410igDjak5L._AC_.jpg"),
-    new Product(5, "Senior Shopping Cart", "This is a shoppping cart intended to be used by Seniors.", 120.0, "https://m.media-amazon.com/images/I/71HhPBwMYPL._AC_SL1500_.jpg"),
-    new Product(6, "Bubbles", "Bubbles", 9000.0)
-  ];
+  products: Product[] = [];
 
   configUrl="http://localhost:9080/inventory-management/inventory";
 
@@ -32,10 +25,10 @@ export class ProductService {
 
   getData() {
     this.http.get(this.configUrl).subscribe((data: any) => {
-      console.log(data);
-      data.forEach((element: any) => {
-        console.log(element.url);
-      });
+      for (let item of data.itemList){
+        this.products.push(new Product(item.pid, item.name, item.description, item.price, item.url));
+      }
     });
+    console.log(this.products)
   }
 }
