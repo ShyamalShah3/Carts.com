c46dd5d037defba051f7ce16b620bd81e726d81f
Shyamal Shah <47264079+ShyamalShah3@users.noreply.github.com>
2021-11-09 00:39:08 -0500

    Using Payment Processing

--- Carts/src/app/components/payment/payment-page.component.ts
+++ Carts/src/app/components/payment/payment-page.component.ts
@@ -1,9 +1,11 @@
-
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Injectable, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 import { paymentInfo } from 'src/app/models/paymentInfo';
+import { ShippingInfo } from 'src/app/models/shipping-info';
 import { PaymentInfoService } from 'src/app/services/payment.service';
+import { ShippingInfoService } from 'src/app/services/shipping-info.service';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
 
 
 @Component({
@@ -11,13 +13,23 @@ import { PaymentInfoService } from 'src/app/services/payment.service';
   templateUrl: './payment-page.component.html',
   styleUrls: ['./payment-page.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
+
 export class PaymentPageComponent implements OnInit {
 
+  @Injectable()
+
   private payInfo:paymentInfo ;
+  private myShipping : ShippingInfo;
+  configUrl="http://localhost:9080/payment-processing/credit-card-processing/payment";
+
   @Input('ngModel') payForm:FormGroup;
 
 
-  constructor(private service: PaymentInfoService) {
+  constructor(private service: PaymentInfoService, private shipping: ShippingInfoService, private http: HttpClient) {
+    this.myShipping = shipping.shippingInfo
     this.payInfo = service.pInfo;
     let cardNumRegex :string = "[0-9]{16}"
     let CVVRegex : string  = "[0-9]{3}"
@@ -34,7 +46,8 @@ export class PaymentPageComponent implements OnInit {
 
   ngOnInit(): void {
     
-     }
+  }
+
   private packagePaymentInfo() {
     this.payInfo.CardName = this.cardNameForm?.value;
     this.payInfo.CardNum = this.cardNumForm?.value;
@@ -44,7 +57,31 @@ export class PaymentPageComponent implements OnInit {
 
   public onNextClick() {
     this.packagePaymentInfo();
+    this.postData();
   }
+
+  postData(){
+    let data = {
+      "cardName": this.payInfo.CardName,
+      "cardNum": this.payInfo.CardNum,
+      "expiration": this.payInfo.ExpD,
+      "cvv": this.payInfo.cvv
+    }
+    let result = from( // wrap the fetch in a from if you need an rxjs Observable
+      fetch(
+        this.configUrl,
+        {
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST', 
+          mode: 'no-cors'
+        }
+      )
+    );
+    result.subscribe(x => console.log(x));
+  }   
 
   get cardNameForm(){
     return this.payForm.get("nameOnCard");
