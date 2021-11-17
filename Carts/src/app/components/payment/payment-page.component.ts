56bf6b6dfc9a6dee4f4c09b04aa3980065d96ff3
Hank Liu <liu.7220@buckeyemail.osu.edu>
2021-11-17 09:48:02 -0500

    Revert "Using Payment Processing"
    This reverts commit c46dd5d037defba051f7ce16b620bd81e726d81f.

--- Carts/src/app/components/payment/payment-page.component.ts
+++ Carts/src/app/components/payment/payment-page.component.ts
@@ -1,11 +1,9 @@
-import { Component, Inject, Injectable, Input, OnInit } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { paymentInfo } from 'src/app/models/paymentInfo';
-import { ShippingInfo } from 'src/app/models/shipping-info';
 import { PaymentInfoService } from 'src/app/services/payment.service';
-import { ShippingInfoService } from 'src/app/services/shipping-info.service';
-import { HttpClient } from '@angular/common/http';
-import { from } from 'rxjs';
 
 
 @Component({
@@ -13,23 +11,13 @@ import { from } from 'rxjs';
   templateUrl: './payment-page.component.html',
   styleUrls: ['./payment-page.component.css']
 })
-@Injectable({
-  providedIn: 'root'
-})
-
 export class PaymentPageComponent implements OnInit {
 
-  @Injectable()
-
   private payInfo:paymentInfo ;
-  private myShipping : ShippingInfo;
-  configUrl="http://localhost:9080/payment-processing/credit-card-processing/payment";
-
   @Input('ngModel') payForm:FormGroup;
 
 
-  constructor(private service: PaymentInfoService, private shipping: ShippingInfoService, private http: HttpClient) {
-    this.myShipping = shipping.shippingInfo
+  constructor(private service: PaymentInfoService) {
     this.payInfo = service.pInfo;
     let cardNumRegex :string = "[0-9]{16}"
     let CVVRegex : string  = "[0-9]{3}"
@@ -46,8 +34,7 @@ export class PaymentPageComponent implements OnInit {
 
   ngOnInit(): void {
     
-  }
-
+     }
   private packagePaymentInfo() {
     this.payInfo.CardName = this.cardNameForm?.value;
     this.payInfo.CardNum = this.cardNumForm?.value;
@@ -57,31 +44,7 @@ export class PaymentPageComponent implements OnInit {
 
   public onNextClick() {
     this.packagePaymentInfo();
-    this.postData();
   }
-
-  postData(){
-    let data = {
-      "cardName": this.payInfo.CardName,
-      "cardNum": this.payInfo.CardNum,
-      "expiration": this.payInfo.ExpD,
-      "cvv": this.payInfo.cvv
-    }
-    let result = from( // wrap the fetch in a from if you need an rxjs Observable
-      fetch(
-        this.configUrl,
-        {
-          body: JSON.stringify(data),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST', 
-          mode: 'no-cors'
-        }
-      )
-    );
-    result.subscribe(x => console.log(x));
-  }   
 
   get cardNameForm(){
     return this.payForm.get("nameOnCard");
